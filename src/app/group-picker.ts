6fc0e99f3d3eb1a61e98d955b1613ba9b43719cf
martinjoconnor <martin@zippyrate.com>
2026-09-18 21:36:17 -0700

    Disable browser spellchecking throughout Workspace

--- src/app/group-picker.ts
+++ src/app/group-picker.ts
@@ -14,6 +14,7 @@ import { FormsModule } from "@angular/forms";
   template: `
     <label [for]="id + '-input'">{{ label }}</label>
     <input
+      spellcheck="false"
       [id]="id + '-input'"
       type="text"
       role="combobox"
